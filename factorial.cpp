916bdb7b16b7d96483297bbeef8ff59c92543678
Mahmood Hussain <md.hussain1818@gmail.com>
2021-08-12 06:32:36 -0700

    rehashed project with visual studio

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+double fact(int n) {
+
+    if (n == 1) {
+        return 1;
+    }
+    else
+        return n * fact(n - 1); 
+}
+
+int main(){
+
+    int num; 
+    cout<<"Please enter a number for factorial : ";
+    cin >> num; 
+    cout << "\nFactorial of Number is : " << fact(num) << endl; 
+    return 0;
+}
