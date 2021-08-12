304a966afa5c51e22fc9245bed0505f44beb2e33
Mahmood Hussain <md.hussain1818@gmail.com>
2021-08-12 07:12:41 -0700

    arrays

---
+++ sortingAlgorithms2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(){
+
+}
+
+int main() {
+
+    int arr[10] = { 2,1,3,7,5,4,9,8,0,10 };
+    for (int i = 0; i < 10; i++) {
+        cout << "Array before : " << arr[i]<<endl;
+    }
+    //cin >> num;
+    //cout << "\nFactorial of Number is : " << fact(num) << endl;
+    return 0;
+}
